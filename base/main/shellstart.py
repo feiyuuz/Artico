b34492b49eb62a8b430fd57f8462627cf27cd9db
FeFe <corporativo.mirtop@gmail.com>
2024-02-28 15:21:05 -0300

    28/2

---
+++ base/main/shellstart.py
@@ -0,0 +1,4 @@
+# This python script its for JUST testing that python works correctly on other machines.
+import time 
+print("Welcome to Ártico, this script seems to work correctly.")
+print("[ O ] Sending request...")
