982421926d0c2940f2bf8b19e3f4e9609178c39b
FeFe <corporativo.mirtop@gmail.com>
2024-02-28 16:37:49 -0300

    2

---
+++ base/main/shell.py
@@ -0,0 +1,25 @@
+import sys
+
+from PyQt5.QtWidgets import (
+    QApplication, QDialog, QMainWindow, QMessageBox
+)
+from PyQt5.uic import loadUi
+
+from desktop import Ui_MainWindow
+
+class Window(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.connectSignalsSlots()
+
+    def connectSignalsSlots(self):
+        self.action_Exit.triggered.connect(self.close)
+        self.action_Find_Replace.triggered.connect(self.findAndReplace)
+        self.action_About.triggered.connect(self.about)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec())
